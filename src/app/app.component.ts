d489c0f855101a31f5b1c6261564919c94e249f3
Shubh <shubhamshukla@rupeeseed.com>
2022-08-03 19:00:44 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Formarray';
+export class AppComponent implements OnInit {
+  productForm: FormGroup = new FormGroup({});
+  products: FormArray | undefined;
+
+  formValues: string = '';
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.addProduct();
+  }
+
+  initializeForm() {
+    this.productForm = this.formBuilder.group({
+      products: this.formBuilder.array([]),
+    });
+  }
+
+  productGetter(): FormArray {
+    return this.productForm?.get('products') as FormArray;
+  }
+
+  newProduct(): FormGroup {
+    return this.formBuilder.group({
+      productName: ['', Validators.required],
+      brand: ['', Validators.required],
+      quantity: ['', Validators.required],
+    });
+  }
+
+  addProduct() {
+    this.productGetter().push(this.newProduct());
+  }
+
+  removeProduct(index: number) {
+    this.productGetter().removeAt(index);
+  }
+
+  onSubmit() {
+    this.formValues = JSON.stringify(this.productForm?.value, null, '\t');
+    console.log('Value on submit ::', this.productForm?.value);
+  }
 }
